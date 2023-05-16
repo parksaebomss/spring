85166cbf83b88f9a00fdf35c3f00342ac4faa1d2
parksaebomss <128570083+parksaebomss@users.noreply.github.com>
2023-05-16 06:24:52 +0000

    조건문 끝 (스위치, 브릭)

--- .ccls-cache/@home@runner@spring/mission.c
+++ .ccls-cache/@home@runner@spring/mission.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-main() {
-  int x;
-  printf("당신의 점수를 입력하세요.\n");
-  scanf("%d", &x);
+// #include <stdio.h>
+// main() {
+//   int x;
+//   printf("당신의 점수를 입력하세요.\n");
+//   scanf("%d", &x);
 
-  if (x >= 90)
-    printf("A학점");
+//   if (x >= 90)
+//     printf("A학점");
 
-  else if (x >= 80)
-    printf("B학점");
+//   else if (x >= 80)
+//     printf("B학점");
 
-  else if (x >= 70)
-    printf("C학점");
+//   else if (x >= 70)
+//     printf("C학점");
 
-  else if (x >= 60)
-    printf("D학점");
+//   else if (x >= 60)
+//     printf("D학점");
 
-  else if (x < 60)
-    printf("F학점");
-}
+//   else if (x < 60)
+//     printf("F학점");
+// }
